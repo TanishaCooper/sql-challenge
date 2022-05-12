07b006d211b552cc3c8d5af58c23284b635672c2
Tanisha Cooper <strickland.tanisha@yahoo.com>
2022-05-12 08:34:46 -0400

    DATA ANALYSIS QUERIES ADDED

--- EmployeeSQL/SQLQuery/EmployeesQuery.sql
+++ EmployeeSQL/SQLQuery/EmployeesQuery.sql
@@ -43,3 +43,27 @@ CREATE TABLE "salaries" (
 );
 
 
+-- Data Analysis
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT EMPLOYEES.EMP_NO AS "EMP_NO", EMPLOYEES.LAST_NAME AS "LAST_NAME", EMPLOYEES.FIRST_NAME AS "FIRST_NAME", EMPLOYEES.SEX AS "SEX", 
+CAST(SALARIES.SALARY AS MONEY) AS "SALARY"
+FROM EMPLOYEES
+JOIN SALARIES ON EMPLOYEES.EMP_NO = SALARIES.EMP_NO;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT FIRST_NAME AS "FIRST_NAME", LAST_NAME AS "LAST_NAME", HIRE_DATE AS "HIRE_DATE"
+FROM EMPLOYEES
+WHERE CAST(EMPLOYEES.HIRE_DATE AS VARCHAR) LIKE '1986%';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT DEPT_MANAGER.DEPT_NO AS "DEPT_NO", DEPARTMENTS.DEPT_NAME AS "DEPT_NAME", DEPT_MANAGER.EMP_NO AS "EMP_NO", EMPLOYEES.LAST_NAME AS "LAST_NAME", 
+EMPLOYEES.FIRST_NAME AS "FIRST_NAME"
+FROM DEPT_MANAGER
+JOIN DEPARTMENTS ON DEPT_MANAGER.DEPT_NO = DEPARTMENTS.DEPT_NO
+JOIN EMPLOYEES ON DEPT_MANAGER.EMP_NO = EMPLOYEES.EMP_NO;
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT EMPLOYEES.EMP_NO AS "EMP_NO", EMPLOYEES.LAST_NAME AS "LAST_NAME", EMPLOYEES.FIRST_NAME AS "FIRST_NAME", DEPARTMENTS.DEPT_NAME AS "DEPT_NAME"
+FROM EMPLOYEES
+JOIN DEPT_EMP ON EMPLOYEES.EMP_NO = DEPT_EMP.EMP_NO
+JOIN DEPARTMENTS ON DEPT_EMP.DEPT_NO = DEPARTMENTS.DEPT_NO;
